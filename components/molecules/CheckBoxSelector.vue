0fbcd6336c1c86c0454f52cfd6cae816e1e29bda
Mia Sinek && Trinity Takei <m14tr1n1ty@gmail.com>
2020-05-18 04:23:59 +0200

    Temporarily remove links on Checkbox/Radiobutton selector rows - they are anchorind to the top. Thanks to @tiagopadrela for the catch

--- components/molecules/CheckBoxSelector.vue
+++ components/molecules/CheckBoxSelector.vue
@@ -1,37 +1,35 @@
 <template>
   <li>
-    <a
+    <!-- <a
       href="#"
       class="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out"
-    >
-      <div class="flex items-center px-4 py-4 sm:px-6">
-        <div class="min-w-0 flex-1 flex items-center">
-          <div class="min-w-0 flex-1 px-4 md:grid md:gap-4">
-            <div>
-              <div
-                class="text-sm leading-5 font-medium text-indigo-600 truncate"
-              >
-                {{ itemName }}
-              </div>
-              <div
-                class="mt-2 flex items-center text-sm leading-5 text-gray-500 max-w-lg truncate pr-10"
-              >
-                <span>{{ description }}</span>
-              </div>
+    > -->
+    <div class="flex items-center px-4 py-4 sm:px-6">
+      <div class="min-w-0 flex-1 flex items-center">
+        <div class="min-w-0 flex-1 px-4 md:grid md:gap-4">
+          <div>
+            <div class="text-sm leading-5 font-medium text-indigo-600 truncate">
+              {{ itemName }}
+            </div>
+            <div
+              class="mt-2 flex items-center text-sm leading-5 text-gray-500 max-w-lg truncate pr-10"
+            >
+              <span>{{ description }}</span>
             </div>
           </div>
         </div>
-        <div>
-          <input
-            id="comments"
-            type="checkbox"
-            class="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
-            :checked="checked"
-            @change.stop.prevent="updateCheckboxState"
-          />
-        </div>
       </div>
-    </a>
+      <div>
+        <input
+          id="comments"
+          type="checkbox"
+          class="form-checkbox h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+          :checked="checked"
+          @change.stop.prevent="updateCheckboxState"
+        />
+      </div>
+    </div>
+    <!-- </a> -->
   </li>
 </template>
 
--- components/molecules/RadioButtonSelector.vue
+++ components/molecules/RadioButtonSelector.vue
@@ -1,43 +1,41 @@
 <template>
   <li>
-    <a
+    <!-- <a
       href="#"
       class="block hover:bg-gray-50 focus:outline-none focus:bg-gray-50 transition duration-150 ease-in-out"
-    >
-      <div class="flex items-center px-4 py-4 sm:px-6">
-        <div class="min-w-0 flex-1 flex items-center">
-          <div class="flex-shrink-0">
-            <slot></slot>
-          </div>
-          <div class="min-w-0 flex-1 px-4 md:grid md:gap-4 ">
-            <div>
-              <div
-                class="text-sm leading-5 font-medium text-indigo-600 truncate"
-              >
-                {{ itemName }}
-              </div>
-              <div
-                class="mt-2 flex items-center text-sm leading-5 text-gray-500 max-w-lg truncate pr-10"
-              >
-                <span>{{ description }}</span>
-              </div>
-            </div>
-            <div class="hidden md:block"></div>
-          </div>
+    > -->
+    <div class="flex items-center px-4 py-4 sm:px-6">
+      <div class="min-w-0 flex-1 flex items-center">
+        <div class="flex-shrink-0">
+          <slot></slot>
         </div>
-        <div>
-          <input
-            :id="itemName"
-            v-model="selectedItem"
-            type="radio"
-            :name="group"
-            :value="itemName"
-            class="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
-            @change.stop.prevent="updateRadioButtonState"
-          />
+        <div class="min-w-0 flex-1 px-4 md:grid md:gap-4 ">
+          <div>
+            <div class="text-sm leading-5 font-medium text-indigo-600 truncate">
+              {{ itemName }}
+            </div>
+            <div
+              class="mt-2 flex items-center text-sm leading-5 text-gray-500 max-w-lg truncate pr-10"
+            >
+              <span>{{ description }}</span>
+            </div>
+          </div>
+          <div class="hidden md:block"></div>
         </div>
       </div>
-    </a>
+      <div>
+        <input
+          :id="itemName"
+          v-model="selectedItem"
+          type="radio"
+          :name="group"
+          :value="itemName"
+          class="form-radio h-4 w-4 text-indigo-600 transition duration-150 ease-in-out"
+          @change.stop.prevent="updateRadioButtonState"
+        />
+      </div>
+    </div>
+    <!-- </a> -->
   </li>
 </template>
 
