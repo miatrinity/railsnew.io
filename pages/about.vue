021252e5060abe4bf995b9c31eced4124fd0ca57
Mia Sinek && Trinity Takei <m14tr1n1ty@gmail.com>
2020-05-22 19:43:07 +0200

    Add some more content, still unfinished

--- pages/about.vue
+++ pages/about.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="max-w-2xl py-0 px-6 mx-auto mt-16 mb-20">
     <paragraph>
-      Hi 👋! railsnew.io was created by us: Mia Sinek and Trinity Takei. We are
-      an indie maker couple currently living in sunny Valencia, Spain 🇪🇸. If you
-      are in the area, drop by for a ☕️ (it's on us)!
+      railsnew.io was originally created by us: Mia Sinek and Trinity Takei. Hi
+      👋! We are an indie maker couple currently living in sunny Valencia, Spain
+      🇪🇸. If you are in the area, drop by for a ☕️ (it's on us)!
     </paragraph>
 
     <paragraph>
@@ -14,6 +14,32 @@
       After working for almost two decades on everyone else’s dreams, we are
       ready to pursue our own. railsnew.io is our first baby step, but there’s
       definitely more to come - stay tuned!
+    </paragraph>
+
+    <paragraph>
+      If you have any feedback/suggestion/just want to say hi, do get in touch -
+      we'd ❤️ to hear from you! Here are a few ways to contact us:
+      <ul>
+        <li>
+          Shoot us an 📧 at
+          <a href="mailto:mia.trinity.codes@gmail.com"
+            >mia.trinity.codes@gmail.com</a
+          >
+        </li>
+        <li>
+          Tweet at
+          <a href="https://twitter.com/TrinityTakei">@trinitytakei</a> (<a
+            href="https://twitter.com/MiaSinek"
+            >@mia</a
+          >
+          is on a digital📟 detox🥦🥬🥒 for now ¯\_(ツ)_/¯)
+        </li>
+        <li>
+          Follow us on
+          <a href="https://gist.github.com/miatrinity">miatrinity@GitHub</a>
+        </li>
+        <li>Coming soon: a brand new blog</li>
+      </ul>
     </paragraph>
   </div>
 </template>
