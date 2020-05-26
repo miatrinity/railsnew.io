e008a167b3a0d4ab9ddb9e0e471b7de1f0adc05d
Mia Sinek && Trinity Takei <m14tr1n1ty@gmail.com>
2020-05-26 03:19:24 +0200

    Cleaned up verification instructions for Tailwind + Stimulus (Reflex)

--- pages/stimulus-reflex-tailwind-verify.vue
+++ pages/stimulus-reflex-tailwind-verify.vue
@@ -1,9 +1,11 @@
 <template>
-  <div class="max-w-2xl py-0 px-6 mx-auto mt-16 mb-20">
+  <div
+    class="max-w-2xl py-0 px-6 mx-auto mt-0 mb-20 md:mt-10 lg:mt-16 xl:mt-18 mb-20"
+  >
     <h1
       class="text-4xl font-semibold antialiased text-gray-700 tracking-tight leading-tight mb-6"
     >
-      Verify your Stimulus+Tailwind Setup
+      Verify your Stimulus Reflex+Tailwind Setup
     </h1>
     <ol class="list-decimal leading-loose">
       <li>cd into the generated app</li>
@@ -11,38 +13,60 @@
       <li>
         paste this into
         <span class="italic">app/views/verify/index.html.erb</span>:
-        <pre
-          class="hljs"
-          style="display: block; overflow-x: auto; padding: 0.5em; background: rgb(39, 40, 34) none repeat scroll 0% 0%; color: rgb(221, 221, 221);"
-        ><span class="xml">        <span class="hljs-tag" style="color: rgb(249, 38, 114);">&lt;<span class="hljs-name" style="color: rgb(249, 38, 114);">a</span> <span class="hljs-attr">href</span>=<span class="hljs-string" style="color: rgb(166, 226, 46);">"#"</span>
-          <span class="hljs-attr">data-reflex</span>=<span class="hljs-string" style="color: rgb(166, 226, 46);">"click-&gt;CounterReflex#increment"</span>
-          <span class="hljs-attr">data-step</span>=<span class="hljs-string" style="color: rgb(166, 226, 46);">"1"</span> 
-          <span class="hljs-attr">data-count</span>=<span class="hljs-string" style="color: rgb(166, 226, 46);">"&lt;%=</span></span></span><span class="ruby"> @count.to_i </span><span class="xml"><span class="hljs-tag" style="color: rgb(249, 38, 114);"><span class="hljs-string" style="color: rgb(166, 226, 46);">%&gt;"</span>
-          <span class="hljs-attr">class</span>=<span class="hljs-string" style="color: rgb(166, 226, 46);">"underline"</span>
-        &gt;</span>Click me to increment the number:
-          <span class="hljs-tag" style="color: rgb(249, 38, 114);">&lt;<span class="hljs-name" style="color: rgb(249, 38, 114);">span</span> <span class="hljs-attr">class</span>=<span class="hljs-string" style="color: rgb(166, 226, 46);">"text-red-500 text-2xl"</span>&gt;</span> <span class="hljs-tag" style="color: rgb(249, 38, 114);">&lt;<span class="hljs-name" style="color: rgb(249, 38, 114);">%=</span></span></span><span class="ruby"> @count.to_i </span><span class="xml"><span class="hljs-tag" style="color: rgb(249, 38, 114);">%&gt;</span><span class="hljs-tag" style="color: rgb(249, 38, 114);">&lt;/<span class="hljs-name" style="color: rgb(249, 38, 114);">span</span>&gt;</span>
-        <span class="hljs-tag" style="color: rgb(249, 38, 114);">&lt;/<span class="hljs-name" style="color: rgb(249, 38, 114);">a</span>&gt;</span></span></pre>
+        <div
+          style="background: #272822; overflow:auto;width:auto;padding:.2em .6em;"
+          class="px-5 my-5"
+        >
+          <pre
+            style="margin: 0; line-height: 125%"
+          ><span style="color: #f92672">&lt;a</span> <span style="color: #a6e22e">href=</span><span style="color: #e6db74">&quot;#&quot;</span>
+  <span style="color: #a6e22e">data-reflex=</span><span style="color: #e6db74">&quot;click-&gt;CounterReflex#increment&quot;</span>
+  <span style="color: #a6e22e">data-step=</span><span style="color: #e6db74">&quot;1&quot;</span> 
+  <span style="color: #a6e22e">data-count=</span><span style="color: #e6db74">&quot;&lt;%= @count.to_i %&gt;&quot;</span>
+  <span style="color: #a6e22e">class=</span><span style="color: #e6db74">&quot;underline&quot;</span>
+<span style="color: #f92672">&gt;</span><span style="color: #eee">
+  Click me to increment the number:</span>
+  <span style="color: #f92672">&lt;span</span> <span style="color: #a6e22e">class=</span><span style="color: #e6db74">&quot;text-red-500 text-2xl&quot;</span><span style="color: #f92672">&gt;</span>
+    <span style="color: #f92672">&lt;%= </span><span style="color: #eee">@count.to_i</span><span style="color: #f92672"> %&gt;</span>
+  <span style="color: #f92672">&lt;/span&gt;</span>
+<span style="color: #f92672">&lt;/a&gt;</span>
+</pre>
+        </div>
       </li>
+      <li>create <span class="italic">app/reflexes/counter_reflex.rb</span></li>
       <li>
-        create app/reflexes/counter_reflex.rb
-      </li>
-      <li>
-        paste this into app/reflexes/counter_reflex.rb:
+        paste this into
+        <span class="italic">app/reflexes/counter_reflex.rb</span>:
 
-        <pre>
-          class CounterReflex &lt; StimulusReflex::Reflex
-            def increment
-              @count = element.dataset[:count].to_i + element.dataset[:step].to_i
-            end
-          end
-        </pre>
+        <div
+          style="background: #272822; overflow:auto;width:auto;padding:.2em .6em;"
+          class="px-5 my-5"
+        >
+          <pre
+            style="margin: 0; line-height: 125%"
+          ><span style="color: #66d9ef">class</span> <span style="color: #a6e22e">CounterReflex</span> <span style="color: #f92672">&lt;</span> <span style="color: #e6db74">StimulusReflex</span><span style="color: #f8f8f2">:</span><span style="color: #e6db74">:Reflex</span>
+  <span style="color: #66d9ef">def</span> <span style="color: #a6e22e">increment</span>
+    <span style="color: #f8f8f2">@count</span> <span style="color: #f92672">=</span> <span style="color: #f8f8f2">element</span><span style="color: #f92672">.</span><span style="color: #f8f8f2">dataset</span><span style="color: #f92672">[</span><span style="color: #e6db74">:count</span><span style="color: #f92672">].</span><span style="color: #f8f8f2">to_i</span> <span style="color: #f92672">+</span> <span style="color: #f8f8f2">element</span><span style="color: #f92672">.</span><span style="color: #f8f8f2">dataset</span><span style="color: #f92672">[</span><span style="color: #e6db74">:step</span><span style="color: #f92672">].</span><span style="color: #f8f8f2">to_i</span>
+  <span style="color: #66d9ef">end</span>
+<span style="color: #66d9ef">end</span>
+</pre>
+        </div>
       </li>
-      <li>run webpack-dev-server</li>
-      <li>run rails -s</li>
-      <li>hit http://localhost:3000/verify/index</li>
+      <li>> <code>bin/webpack-dev-server</code></li>
+      <li>> <code>bin/rails s</code></li>
+      <li>
+        Visit
+        <a
+          href="http://localhost:3000/verify/index"
+          class="underline text-indigo-800"
+          >http://localhost:3000/verify/index</a
+        >
+      </li>
+      <li>
+        The number should be red (verifies that TailwindCSS works) and should be
+        incrementing when you click the "Click me to increment the number:" link
+        (verifies that Stimulus works)
+      </li>
     </ol>
-
-    The number should be red and should be incrementing when you click the
-    "Click me to increment the number:" link
   </div>
 </template>
--- pages/stimulus-tailwind-verify.vue
+++ pages/stimulus-tailwind-verify.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="max-w-2xl py-0 px-6 mx-auto mt-16 mb-20">
+  <div
+    class="max-w-2xl py-0 px-6 mx-auto mt-0 mb-20 md:mt-10 lg:mt-16 xl:mt-18 mb-20"
+  >
     <h1
       class="text-4xl font-semibold antialiased text-gray-700 tracking-tight leading-tight mb-6"
     >
