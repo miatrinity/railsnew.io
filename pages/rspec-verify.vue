f34ac1ca13cfddbdc6301a33ece239b13a72cac3
Mia Sinek && Trinity Takei <mia.trinity.codes@gmail.com>
2020-08-14 04:41:24 +0200

    RSPec verify page

---
+++ pages/rspec-verify.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    class="max-w-2xl py-0 px-6 mx-auto mt-0 mb-20 md:mt-10 lg:mt-16 xl:mt-18 mb-20"
+  >
+    <h1
+      class="text-4xl font-semibold antialiased text-gray-700 tracking-tight leading-tight mb-6"
+    >
+      Verify your RSpec Setup
+    </h1>
+    <ol class="list-decimal leading-loose">
+      <li>cd into the generated app</li>
+      <li><pre>> rspec</pre></li>
+      <li>
+        You should see something like
+
+        <pre>
+          
+No examples found.
+
+Finished in 0.00022 seconds (files took 0.06779 seconds to load)
+0 examples, 0 failures            
+        </pre>
+      </li>
+    </ol>
+  </div>
+</template>
